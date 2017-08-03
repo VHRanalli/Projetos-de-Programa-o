8e969574828a34e04401976d1e38e24296f92c00
Victor Henrique Ranalli Barbosa <vhvictor978@gmail.com>
2017-08-02 23:00:26 -0300

    Add files via upload

---
+++ Calculadora_do_IMC/src/br/com/senac/ICM/CalculodoIcm.java
@@ -0,0 +1,18 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senac.ICM;
+
+/**
+ *
+ * @author Victor
+ */
+public class CalculodoIcm {
+    public static void main(String[] args) {
+        //chamar o formulario
+        Icm calculadora = new Icm();
+        calculadora.setVisible(true);
+    }
+}
